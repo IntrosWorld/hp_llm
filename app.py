8bbc0439981031113932c83b9eed1c93aa8c6ab9
IntrosWorld <50764469+IntrosWorld@users.noreply.github.com>
2025-06-07 01:45:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ with open(CHUNKS_FILE, "rb") as f:
 # Load embedding model
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 
-# Load fine-tuned GPT-2 model
+# Load finetuned GPT2 model
 model = GPT2LMHeadModel.from_pretrained("finetuned_hp")
 tokenizer = GPT2Tokenizer.from_pretrained("finetuned_hp")
 tokenizer.pad_token = tokenizer.eos_token
 generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
-#chatbot
+#Chatbot
 def rag_chat(question, chat_history):
     question_embedding = embedding_model.encode([question]).astype("float32")
 
@@ -54,11 +54,11 @@ Answer:"""
     chat_history.append((question, answer_only))
     return chat_history, chat_history
 
-#text generator
+#Text generator
 def simple_text_generation(prompt_text):
     result = generator(
         prompt_text,
-        max_length=100,
+        max_new_tokens=50,
         do_sample=True,
         temperature=0.9,
         top_k=50,
@@ -76,7 +76,7 @@ with gr.Blocks() as demo:
     # Chatbot UI 
     with gr.Group(visible=False) as chatbot_group:
         chatbot_ui = gr.Chatbot()
-        msg = gr.Textbox(label="Ask a question...")
+        msg = gr.Textbox(label="Ask a question...",placeholder="Example: What was magic according to Harry ?")
         clear_chat = gr.Button("Clear Chat")
         chat_state = gr.State([])
 
@@ -88,7 +88,7 @@ with gr.Blocks() as demo:
 
     #Text Generator UI
     with gr.Group(visible=True) as textgen_group:
-        text_prompt = gr.Textbox(label="Enter some text so gpt can continue it")
+        text_prompt = gr.Textbox(label="Enter some text so Finetuned GPT2 can continue it",placeholder="Example: Harry opened the door and")
         generate_btn = gr.Button("Generate Text")
         generated_output = gr.Textbox(label="Generated Output")
 
